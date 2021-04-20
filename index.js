222042dc3cbad2210080a05e2ae1875965d872cd
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2021-04-20 09:31:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,31 @@ function hideAll() {
 }
 
 function band() {
+  document.getElementById("bandPlaylist").style.display = "block";
+  document.getElementById("pokemonPlaylist").style.display = "none";
+  document.getElementById("marioLuigiPlaylist").style.display = "none";
+  document.getElementById("fridayNightPlaylist").style.display = "none";
 }
 
 function pokemon() {
+  document.getElementById("bandPlaylist").style.display = "none";
+  document.getElementById("pokemonPlaylist").style.display = "block";
+  document.getElementById("marioLuigiPlaylist").style.display = "none";
+  document.getElementById("fridayNightPlaylist").style.display = "none";
 }
 
 function marioLuigi() {
+  document.getElementById("bandPlaylist").style.display = "none";
+  document.getElementById("pokemonPlaylist").style.display = "none";
+  document.getElementById("marioLuigiPlaylist").style.display = "block";
+  document.getElementById("fridayNightPlaylist").style.display = "none";
 }
 
 function fridayNight() {
+  document.getElementById("bandPlaylist").style.display = "none";
+  document.getElementById("pokemonPlaylist").style.display = "none";
+  document.getElementById("marioLuigiPlaylist").style.display = "none";
+  document.getElementById("fridayNightPlaylist").style.display = "block";
 }
 
 hideAll();
