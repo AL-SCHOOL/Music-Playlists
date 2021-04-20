7587b4334f58e1660b51d355bddfff2ea1c9f9b8
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2021-04-20 10:06:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-document.getElementById("pokemonPlaylist").style.display = "none";
-document.getElementById("marioLuigiPlaylist").style.display = "none";
-document.getElementById("fridayNightPlaylist").style.display = "none";
-document.getElementById("bandPlaylist").style.display = "block";
-document.getElementById("bandPlaylist").style.display = "none";
+function hideAll() {
+  document.getElementById("bandPlaylist").style.display = "none";
+  document.getElementById("pokemonPlaylist").style.display = "none";
+  document.getElementById("marioLuigiPlaylist").style.display = "none";
+  document.getElementById("fridayNightPlaylist").style.display = "none";
+}
 
 function band() {
   document.getElementById("bandPlaylist").style.display = "block";
