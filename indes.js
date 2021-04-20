178d3f0b86cf54256a721c73e2b10afcfab61874
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2021-04-20 09:20:32 -0400

    Create indes.js

---
+++ indes.js
@@ -0,0 +1,8 @@
+function hideAll() {
+  document.getElementById("band-playlist").style.display = "none";
+  document.getElementById("pokemon-playlist").style.display = "none";
+  document.getElementById("mario-luigi-playlist").style.display = "none";
+  document.getElementById("friday-night-playlist").style.display = "none";
+}
+
+hideAll();
