11807e63cab1fe5099714702a66e150ab0559f5e
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2021-04-20 13:00:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function home() {
+  document.getElementById("homepage").style.display = "block";
+  document.getElementById("bandPlaylist").style.display = "none";
+  document.getElementById("pokemonPlaylist").style.display = "none";
+  document.getElementById("marioLuigiPlaylist").style.display = "none";
+  document.getElementById("fridayNightPlaylist").style.display = "none";
+  document.getElementById("personalPlaylist").style.display = "none";
+}
+
 function band() {
   document.getElementById("homepage").style.display = "none";
   document.getElementById("bandPlaylist").style.display = "block";
@@ -8,7 +17,7 @@ function band() {
 }
 
 function pokemon() {
-  document.getElementById("homepage").style.display = "none";
+  document.getElementById("homepage").style.display = "block";
   document.getElementById("bandPlaylist").style.display = "none";
   document.getElementById("pokemonPlaylist").style.display = "block";
   document.getElementById("marioLuigiPlaylist").style.display = "none";
