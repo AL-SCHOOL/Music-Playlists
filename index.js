e61c74c36b4787efebb12c9a2480c9eafef2fdfd
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2021-04-23 08:19:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -57,3 +57,13 @@ function personal() {
   document.getElementById("fridayNightPlaylist").style.display = "none";
   document.getElementById("personalPlaylist").style.display = "block";
 }
+
+function hKeyDown() {
+  var H = 72;
+  const keydownHandler = event => {
+    "use strict"
+    if (event.keyCode == H) {
+      document.getElementById("navButton").style.display = "none";
+    }
+  }
+}
